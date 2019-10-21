c20af1b16009e35e4902f6e7606d282c34f3d6e8
Norzarifah Kamarauzaman <55696926+norzarifah@users.noreply.github.com>
2019-10-21 13:17:00 +0800

    Create server.R

---
+++ server.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(leaflet)
+library(geojsonio)
+library(maps)
+library(mapdata)
+
+
+### MISC FUNCTIONS ----
+#source("function1.R")
+#source("function2.R")
+#source("function3.R")
+
+### SERVER FUNCTIONS ----
+server <- function(input, output, session){
+
+  ##### MODULE 1 - DATA CLEANING #####
+  sites <- data_long
+  output$map  <- renderLeaflet({
+    leaflet(sites) %>% 
+      addTiles() %>% 
+      addCircles(lng = ~Longitude, lat = ~Latitude, weight = 1, radius = ~accident_rate * 30, popup = ~States, fillOpacity = 0.5)
+  })
+}
+
